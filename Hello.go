7db89d660c6a3510532b1566b1a5835b3baeb66f
Redneckmonster <45948048+redneckmonster@users.noreply.github.com>
2018-12-17 15:11:13 -0300

    Update Hello.go
    Main and init() together

--- Hello.go
+++ Hello.go
@@ -6,5 +6,15 @@ func main() {
 	init()
 	fmt.Printf("hello, world\n")
 	fmt.Printf("modified today\n")
-	fmt.Printf("modified today\n")
+}
+
+// Function to be called before anything else...
+func init() {
+
+	fmt.Printf("Initializing...\n")
+
+	fmt.Printf("....working....\n")
+
+	fmt.Printf("...init finished succersfully.\n")
+
 }
