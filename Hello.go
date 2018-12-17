4504a097501062ffdf264e70ff811ba533545e34
Julio Pironio <juliopironio@yahoo.com>
2018-12-17 11:29:19 -0300

    Adding New Features file

--- Hello.go
+++ Hello.go
@@ -4,7 +4,12 @@ import "fmt"
 
 func main() {
 	init()
+	Tell()
 	fmt.Printf("hello, world\n")
 	fmt.Printf("modified today\n")
 	fmt.Printf("modified today\n")
+}
+
+func Tell(){
+	fmt.Printf("Tell\n")
 }
---
+++ NewFeatures.go
@@ -0,0 +1,1 @@
+//All new features here
